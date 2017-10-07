50114c30d3d8ada86ea55613893bf5f458cd8089
Olufemi Salaam <salaam.olufemi@gmail.com>
2017-10-07 15:26:34 -0400

    trash'

--- app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
+++ app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.brandon.bisonevents;
 
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.R.id.list;
+import static java.security.AccessController.getContext;
 
 import java.util.ArrayList;
 import java.util.List;
