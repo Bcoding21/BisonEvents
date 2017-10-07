58200e38723f3c7a79a55e15c9069f0689d2332c
Olufemi Salaam <salaam.olufemi@gmail.com>
2017-10-07 14:23:04 -0400

    List view'

--- app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
+++ app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.brandon.bisonevents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> Events = new ArrayList<>();
+        Events.add("PARTY");
+        Events.add("BIGGER PARTY");
+        Events.add("BIGGEST PARTY");
+
+        ListView listm = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Events);
+        listm.setAdapter(adapter);
     }
 }
