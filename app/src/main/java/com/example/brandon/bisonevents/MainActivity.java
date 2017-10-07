859b88fc1b99b66b65d4f84973b6b27197bb40ea
Asim Williams <asimwilliams@Asims-MacBook-Pro.local>
2017-10-07 16:39:47 -0400

    hamburger

--- app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
+++ app/src/main/java/com/example/brandon/bisonevents/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 import static android.R.id.list;
 
@@ -16,11 +19,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+
+        String[] osArray = { "ACM", "WHBC", "NSBE", "BLACKBURN", "CRAMPTON" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
         /*ArrayList<String> Events = new ArrayList<>();
         Events.add("PARTY");
@@ -46,11 +64,13 @@ public class MainActivity extends AppCompatActivity {
         List<Event> eventsList = new ArrayList<Event>();
         for (String[] eventArray : eventArrayList){
             eventsList.add(new Event(eventArray[0], eventArray[1], eventArray[3], eventArray[2]));
+
         }
 
         ArrayAdapter adapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1, eventsList);
-        listm.setAdapter(adapter);
-    }
+        listm.setAdapter(adapter);}
 
 
 }
+
+
